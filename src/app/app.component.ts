50ca7c78eff1ed4297761a7bcdb482856e98b2c9
Vipul Mehta <tovipul21@hotmail.com>
2024-08-12 10:32:48 -0500

    Added a new component - tasks to the project and implemented code/logic to display the name of the selected user.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,18 +3,26 @@ import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
 import { UserComponent } from './user/user.component';
 import { DUMMY_USERS } from './dummy-users';
+import { TasksComponent } from "./tasks/tasks.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeaderComponent, UserComponent, CommonModule],
+  imports: [HeaderComponent, UserComponent, CommonModule, TasksComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
   users = DUMMY_USERS;
+  selectedUserId = "u1";
+
+  get selectedUser(){
+    //return this.users.filter(task => task.id.includes("u1"));
+    return this.users.find(user => user.id === this.selectedUserId);
+  }
 
   onSelectUser(id: string){
     console.log(id);
+    this.selectedUserId = id;
   }
 }
