4ddad6e8bfce5bc72cfd5645ee8143aef9900b6e
Vipul Mehta <tovipul21@hotmail.com>
2024-08-11 23:34:10 -0500

    Implemented code to handle the clock event from the user component.

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,28 +1,38 @@
-import { Component, computed, signal } from '@angular/core';
-import { DUMMY_USERS } from '../dummy-users';
-import { computeMsgId } from '@angular/compiler';
-
-const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
+import { Component, computed, EventEmitter, Input, input, Output } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-user',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './user.component.html',
   styleUrl: './user.component.css'
 })
-export class UserComponent {
-  selectedUser = signal(DUMMY_USERS[randomIndex]);
-  imagePath = computed(() => 'assets/users/' + this.selectedUser().avatar);
-  userList = DUMMY_USERS;
 
-  // get imagePath(){
-  //   return "assets/users/" + this.selectedUser().avatar;
-  // }
+export class UserComponent {
+  // This is an decorator (non Signal) approach
+  @Input({required : true}) id! : string;
+  @Input({required : true}) avatar! : string;
+  @Input({required : true}) name! : string;
+  @Output() select = new EventEmitter();
+
+  get imagePath(){
+    return 'assets/users/' + this.avatar;
+  }
+
+  // This is a Signal based approach which is also an alternative way of using other then decorator approch
+  // avatar = input.required<string>();
+  // name = input.required<string>();
+
+  // imagePath = computed(() => {
+  //     return 'assets/users/' + this.avatar()
+  // });
+
+  get fullName(){
+    return this.name;
+  }
 
   onSelectUser(){
-    console.log('clicked !!');
-    const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
-    this.selectedUser.set(DUMMY_USERS[randomIndex]);
+    this.select.emit(this.id);
   }
 }
