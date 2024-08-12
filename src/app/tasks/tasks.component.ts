7cf01d88477ea3024aaa200a7e152c6f92e84db9
Vipul Mehta <tovipul21@hotmail.com>
2024-08-12 10:34:19 -0500

    Refactored component to display the name of the selected user on the UI.

--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { DUMMY_USERS } from '../dummy-users';
+import { USERS_TASK_LIST } from '../dummy-users';
 
 @Component({
   selector: 'app-tasks',
@@ -7,6 +9,15 @@ import { Component } from '@angular/core';
   templateUrl: './tasks.component.html',
   styleUrl: './tasks.component.css'
 })
-export class TasksComponent {
 
+export class TasksComponent {
+  @Input({required : true}) userName! : string;
+
+  userTaskList = USERS_TASK_LIST;
+  //userTaskListByUser[] = null;
+  
+  getSelectedUserTaskList(){
+    //this.userTaskListByUser = USERS_TASK_LIST.filter(task => task.userId.includes("u1"));
+    return USERS_TASK_LIST.filter(task => task.userId.includes("u1"));
+  }
 }
