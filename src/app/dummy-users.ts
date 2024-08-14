10898d40a5d7047fa75b04c730ae4ae66bee4462
Vipul Mehta <tovipul21@hotmail.com>
2024-08-14 11:27:47 -0500

    Added data.

--- src/app/dummy-users.ts
+++ src/app/dummy-users.ts
@@ -33,9 +33,25 @@ export const DUMMY_USERS = [
 
 export const USERS_TASK_LIST = 
 [
-  {"taskId" : "1", "userId": "u1", "taskName" : "Task 1", "taskStatus" : "Pending"},
-  {"taskId" : "2", "userId": "u1", "taskName" : "Task 2", "taskStatus" : "In Progress"},
-  {"taskId" : "3", "userId": "u2", "taskName" : "Task 3", "taskStatus" : "Completed"},
-  {"taskId" : "4", "userId": "u2", "taskName" : "Task 4", "taskStatus" : "Completed"},
-  {"taskId" : "5", "userId": "u3", "taskName" : "Task 5", "taskStatus" : "Cancelled"}
+  {
+    id: 't1',
+    userId: 'u1',
+    title: 'Master Angular',
+    summary: 'Learn all the basic and advanced features of Angular & how to apply them.',
+    dueDate: '2025-12-31',
+  },
+  {
+    id: 't2',
+    userId: 'u3',
+    title: 'Build first prototype',
+    summary: 'Build a first prototype of the online shop website',
+    dueDate: '2024-05-31',
+  },
+  {
+    id: 't3',
+    userId: 'u3',
+    title: 'Prepare issue template',
+    summary: 'Prepare and describe an issue template which will help with project management',
+    dueDate: '2024-06-15',
+  }
 ];
