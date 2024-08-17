fff5a3a77f88b8de81c61dbb241941f36ad8cd5b
Vipul Mehta <tovipul21@hotmail.com>
2024-08-17 14:41:47 -0500

    Created new properties and binded them to the field on the UI.

--- src/app/tasks/new-task/new-task.component.ts
+++ src/app/tasks/new-task/new-task.component.ts
@@ -1,18 +1,33 @@
 import { Component, EventEmitter, Output } from '@angular/core';
+import { FormsModule, NgModel } from '@angular/forms';
+import { INewTaskData } from '../new-task/new-task.model';
 
 @Component({
   selector: 'app-new-task',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './new-task.component.html',
   styleUrl: './new-task.component.css'
 })
 export class NewTaskComponent {
  
   @Output() cancel = new EventEmitter<void>();
+  @Output() submit = new EventEmitter<INewTaskData>();
+
+  enteredTitle = '';
+  enterdSummary = '';
+  enteredDate = '';
 
   onCancel(){
     this.cancel.emit();
   }
 
+  onFormSubmit(){
+    console.log('Forms submitted from add task form. Now emitting the event to parent.');
+    this.submit.emit({
+      title: this.enteredTitle,
+      summary: this.enterdSummary,
+      dueDate: this.enteredDate
+    });
+  }
 }
